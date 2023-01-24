10a33191b63fd4084409e2780e799facdb082c9d
53911453+brianpavon@users.noreply.github.com <53911453+brianpavon@users.noreply.github.com>
2023-01-24 18:14:46 -0300

    inicio logica para agregar productos

--- api/src/Controllers/ProductosController.php
+++ api/src/Controllers/ProductosController.php
@@ -3,9 +3,13 @@
 namespace Controllers;
 
 use Components\GenericResponse;
-use Models\Producto;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
+
+//Modelos
+use Models\Categoria;
+use Models\Producto;
+use Models\Color;
 
 class ProductosController
 {
@@ -19,7 +23,40 @@ class ProductosController
 
     //Crea un nuevo producto
     public function nuevoProducto(Request $request,Response $response){
-        
+        try {
+            $codigoProd = $request->getParsedBody()['codigoProd'];
+            $nombreProd = $request->getParsedBody()['nombre'];
+            $descripcion = $request->getParsedBody()['descripcion'];
+            $precio = $request->getParsedBody()['precio'];
+            $stock = $request->getParsedBody()['stock'];
+            $codigosColor = $request->getParsedBody()['codigoColor'];
+            $codigosCategoria = $request->getParsedBody()['codigoCategoria'];
+
+            //obtengo los ids de las categorias que se cargaron           
+            foreach ($codigosCategoria as $codCategoria) {
+                $categ = Categoria::where('codigo',$codCategoria)->first();
+                $idsCategoria[] = ['idCateg'=>$categ->id_categ];
+            }
+
+            //obtengo los ids de los colores que se cargaron
+            foreach($codigosColor as $codColor){
+                $color = Color::where('codigo',$codColor)->first();
+                $idsColor[] =["idColor"=>$color->id_color];
+            }
+            
+
+            $nuevoProducto = new Producto;
+            // $nuevoProducto = Producto::create([
+            //     'codigo' => $codigoProd,
+            //     'nombre' => $nombreProd
+            // ]);
+
+            $response->getBody()->write(GenericResponse::obtain(true,"Se creó un nuevo producto",$nuevoProducto));
+            $response->withStatus(200);
+        } catch (\Throwable $th) {
+            $response->getBody()->write(GenericResponse::obtain(false,$th->getMessage(),$th));
+            $response->withStatus(500);
+        }
         return $response;
     }
 
