e3740df9a46f14dfd25c668a4471312f28c9c067
53911453+brianpavon@users.noreply.github.com <53911453+brianpavon@users.noreply.github.com>
2023-03-03 16:43:25 -0300

    inicio conexion con backend para los productos

--- front/src/components/Dashboard/Products/AbmProducts.js
+++ front/src/components/Dashboard/Products/AbmProducts.js
@@ -1,4 +1,18 @@
+import { useEffect, useState } from 'react'
 const AbmProducts = () => {
+    const [data, setData] = useState([]);
+    const url = process.env.REACT_APP_url_server_local;
+    useEffect(() => {
+        fetch(`${url}productos`)
+          .then(
+            response => response.json()
+            )
+          .then(
+                data => {
+                    console.log(data.content);
+            //setData(data);
+            });
+      }, []);
     return(
         <h1>ESTAMOS EN PRODUCTOS</h1>
     )
