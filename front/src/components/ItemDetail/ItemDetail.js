d1761271f4da1b31e4ebeb30c031b5f1448ff421
53911453+brianpavon@users.noreply.github.com <53911453+brianpavon@users.noreply.github.com>
2023-03-15 18:26:54 -0300

    ok mostrar producto por codigo desde el backend

--- front/src/components/ItemDetail/ItemDetail.js
+++ front/src/components/ItemDetail/ItemDetail.js
@@ -3,14 +3,14 @@ import { CartContext } from "../../context/CartContext";
 import { useContext,useState } from "react";
 
 
-const ItemDetail = ({id,name, img, price,description}) =>{
+const ItemDetail = ({codigo,nombre, imagenes, precio,descripcion}) =>{
     const [quantity,setQuantity] = useState(0);
     
     const {addItem} = useContext(CartContext);
     
     const handleOnAdd = (quantity) =>{
         setQuantity(quantity);        
-        addItem({id,name,price,img,quantity});
+        addItem({codigo,nombre,precio,imagenes,quantity});
     }
 
     return(
@@ -22,13 +22,13 @@ const ItemDetail = ({id,name, img, price,description}) =>{
             <div className="card mb-3" style={{maxWidth:'50vw'}}>
                 <div className="row g-0">
                     <div className="col-md-5">
-                        <img src={img} className="img-fluid rounded-start" alt="..."/>
+                        <img src={`${process.env.REACT_APP_url_server_local}${imagenes[0].path_img}`} className="img-fluid rounded-start" alt={`img-${nombre}`}/>
                     </div>
                     <div className="col-md-6 p-4 mt-4">
                         <div className="card-body p-4">
-                            <h3 className="card-title mt-4 mb-4">{name}</h3>
-                            <p className="card-text mt-4 mb-4">{description}</p>
-                            <h5 className="card-title mt-4">${price}</h5>
+                            <h3 className="card-title mt-4 mb-4">{nombre}</h3>
+                            <p className="card-text mt-4 mb-4">{descripcion}</p>
+                            <h5 className="card-title mt-4">${precio}</h5>
                         </div>
                         {/* <>
                           <ItemCount id={id} stock={stock} onAdd={handleOnAdd}/>
