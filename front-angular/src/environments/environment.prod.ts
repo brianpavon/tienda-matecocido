7343afbf2f3e5658d445f6e3e854335aa0b32878
Brian Pavon <brian.pavon@consorcioabierto.com>
2026-06-15 01:12:05 -0300

    fix: renombra Config a config (Laravel no lo leia en Linux case-sensitive)

--- front-angular/src/environments/environment.prod.ts
+++ front-angular/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: '/api',
-  storageUrl: '/storage',
+  apiUrl: 'https://api.matecocidoceramica.com.ar/api',
+  storageUrl: 'https://api.matecocidoceramica.com.ar/storage',
 };
